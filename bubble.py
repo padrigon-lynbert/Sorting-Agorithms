a9389d0bd980211b141648c7a920db265cb4bc12
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-12-12 14:31:07 +0800

    using tuple unpacking

--- bubble.py
+++ bubble.py
@@ -1,21 +1,29 @@
 
 
-lst = [4,9,2,5,3,6,7,1]
+lst = [4,9,2,8,5,3,6,7,1]
 
-def sort(a_list):
+def sortA(a_list):
     for i in range(len(a_list)-1, 0, -1):
         for j in range(i):
             if a_list[j] > a_list[j+1]:
                 
-                c = a_list[j]
-                a_list[j] = a_list[j+1]
-                a_list[j+1] = c
-    
+                #using temp variable
+                # c = a_list[j]
+                # a_list[j] = a_list[j+1]
+                # a_list[j+1] = c
+
+                #using tuple unpacking (which is advised and is native 'only' to python)
+                a_list[j], a_list[j+1] = a_list[j+1], a_list[j]
+                
+                # watch the process
+                # print(a_list)
     return a_list
 
-print(sort(lst))
 
-def test(): 
+
+print(sortA(lst))
+
+def swap_test(): 
     a = 5
     b = 10
 
